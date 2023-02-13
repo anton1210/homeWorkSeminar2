6bebb0e25c953cb422f06e00339fd13d8e951133
Anton Maslov <anton-maslov3@mail.ru>
2023-02-13 18:15:24 +0300

    черновик задачи 13

--- number_task13/Program.cs
+++ number_task13/Program.cs
@@ -1,4 +1,13 @@
 ﻿/* Напишите программу, которая выводит третью цифру заданного числа 
 или сообщает, что третьей цифры нет. (645 -> 5  78 -> третьей цифры нет
 32679 -> 6) */
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 100)
+{
+    Console.WriteLine(num % 10);
+}
+else
+    Console.WriteLine("третьей цифры нет");
