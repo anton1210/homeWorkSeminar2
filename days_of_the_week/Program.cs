7cf8144752afffafaa07c978f86a6b73b674209f
Anton Maslov <anton-maslov3@mail.ru>
2023-02-15 18:12:52 +0300

    days of the week

--- days_of_the_week/Program.cs
+++ days_of_the_week/Program.cs
@@ -1,4 +1,13 @@
 ﻿/* Напишите программу, которая принимает на вход цифру, 
 обозначающую день недели, и проверяет, 
 является ли этот день выходным. (6 -> да    7 -> да 1 -> нет) */
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+Console.WriteLine("Введите цифру дня недели");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+if (weekDay >= 1 && weekDay <= 5)
+    Console.WriteLine("Рабочий день");
+else if (weekDay >= 6 && weekDay <= 7)
+    Console.WriteLine("Выходной");
+else
+    Console.WriteLine("Такого дня недели нет");
