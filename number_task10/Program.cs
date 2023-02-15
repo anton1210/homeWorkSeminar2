7d29c8d255e50ec4fb91eea952624abf020960c7
Anton Maslov <anton-maslov3@mail.ru>
2023-02-15 17:37:43 +0300

    Вторая цифра 3 значиного числа: 2 варианта

--- number_task10/Program.cs
+++ number_task10/Program.cs
@@ -1,4 +1,19 @@
 ﻿/* Напишите программу, которая принимает на вход трёхзначное число 
 и на выходе показывает вторую цифру этого числа.
 (456 -> 5   782 -> 8    918 -> 1) */
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 100 && num < 1000)
+{
+    Console.WriteLine((num / 10) % 10);
+}
+else
+    Console.WriteLine("Введено не трехзначное число");
+
+Console.WriteLine("===============");
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+Console.WriteLine((number / 10) % 10);
